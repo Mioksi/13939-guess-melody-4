c925a281594bb8d30e2a991985a13d32fd2bdb66
Mioksi <mio.natali@hotmail.com>
2020-06-10 01:15:23 +0300

    Добавляет пропсы

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/app/app.jsx';
+import {Settings} from './common/consts';
 
 ReactDOM.render(
-    <App />,
+    <App
+      errorsCount={Settings.ERRORS_COUNT}
+    />,
     document.querySelector(`#root`)
 );

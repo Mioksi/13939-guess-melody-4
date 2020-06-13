70b577312b5e9ca0c5f6543f12deb8eca4c4cd4b
Mioksi <mio.natali@hotmail.com>
2020-06-13 13:50:18 +0300

    Добавляет обработчик клика для кнопки и обновляет снепшот тесты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,9 +3,14 @@ import PropTypes from "prop-types";
 
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
+const welcomeButtonHandler = () => {};
+
 const App = ({errorsCount}) => {
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <WelcomeScreen
+      errorsCount={errorsCount}
+      onWelcomeButtonClick={welcomeButtonHandler}
+    />
   );
 };
 

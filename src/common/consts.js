b97cc00658d3e6aafeb34fad661b92882846e64e
Mioksi <mio.natali@hotmail.com>
2020-06-16 11:19:08 +0300

    Добавляет компонент GenreQuestionScreen

--- src/common/consts.js
+++ src/common/consts.js
@@ -1,5 +1,10 @@
+export const AVATAR_URL = `https://api.adorable.io/avatars/128`;
+
+export const GameType = {
+  ARTIST: `artist`,
+  GENRE: `genre`,
+};
+
 export const Settings = {
   ERRORS_COUNT: 3
 };
-
-export const AVATAR_URL = `https://api.adorable.io/avatars/128`;

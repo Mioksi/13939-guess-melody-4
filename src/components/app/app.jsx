bf44e07c12472b52c5c2857ca81cf07dffaa0a8e
Mioksi <mio.natali@hotmail.com>
2020-06-16 12:41:07 +0300

    Добавляет роутер в App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
-import PropTypes from "prop-types";
+import React, {PureComponent} from 'react';
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import PropTypes from 'prop-types';
 
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
-const welcomeButtonHandler = () => {};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-const App = ({errorsCount}) => {
-  return (
-    <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
-  );
-};
+  render() {
+    const {errorsCount, questions} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderGameScreen()}
+          </Route>
+          <Route exact path="/artist">
+            <ArtistQuestionScreen
+              question={questions[1]}
+              onAnswer={() => {}}
+            />
+          </Route>
+          <Route exact path="/genre">
+            <GenreQuestionScreen
+              question={questions[0]}
+              onAnswer={() => {}}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,

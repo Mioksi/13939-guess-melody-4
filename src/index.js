5c953db3014e8462903b890145b15774531ea897
Mioksi <mio.natali@hotmail.com>
2020-06-28 19:38:03 +0300

    Подключает Redux DevTools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import {Settings} from './common/consts';
 import questions from './mocks/questions';
 import {reducer} from "./reducer.js";
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
     <Provider store={store}>

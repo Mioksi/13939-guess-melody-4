21c7bf6cd943bdf6893bafa899f40b11924f889c
Mioksi <mio.natali@hotmail.com>
2020-07-16 22:21:48 +0300

    Описывает асинхронные action

--- src/reducer/data/data.test.js
+++ src/reducer/data/data.test.js
@@ -1,4 +1,9 @@
-import {reducer, ActionType} from './data';
+import MockAdapter from 'axios-mock-adapter';
+
+import {createAPI} from '../../api';
+import {reducer, ActionType, Operation} from './data';
+
+const api = createAPI(() => {});
 
 const questions = [
   {
@@ -51,5 +56,26 @@ it(`Reducer should update questions by load questions`, () => {
     payload: questions,
   })).toEqual({
     questions,
+  });
+});
+
+describe(`Operation work correctly`, () => {
+  it(`Should make a correct API call to /questions`, function () {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const questionLoader = Operation.loadQuestions();
+
+    apiMock
+      .onGet(`/questions`)
+      .reply(200, [{fake: true}]);
+
+    return questionLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_QUESTIONS,
+          payload: [{fake: true}],
+        });
+      });
   });
 });

b868a96de5a484e4adbde9bd92fd53b39d7a6f91
Mioksi <mio.natali@hotmail.com>
2020-07-17 21:07:39 +0300

    Добавляет обработчик неавторизованного пользователя

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 
-import reducer from './reducer/reducer.js';
-import {Operation as DataOperation} from './reducer/data/data.js';
-import {createAPI} from './api.js';
+import reducer from './reducer/reducer';
+import {Operation as DataOperation} from './reducer/data/data';
+import {Operation as UserOperation, ActionCreator} from './reducer/user/user';
+import {createAPI} from './api';
+
+import {AuthorizationStatus} from './common/consts';
 
 import App from './components/app/app.jsx';
 
-const api = createAPI(() => {});
+const onUnauthorized = () => {
+  store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+};
+
+const api = createAPI(onUnauthorized);
 
 const store = createStore(
     reducer,
@@ -18,6 +25,7 @@ const store = createStore(
 );
 
 store.dispatch(DataOperation.loadQuestions());
+store.dispatch(UserOperation.checkAuth());
 
 ReactDOM.render(
     <Provider store={store}>
